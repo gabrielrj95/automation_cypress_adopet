354bf49d0715dd2f58d4683af9ef51abe092a288
unknown <gabriel.justin95@outlook.com>
2025-04-22 20:26:59 -0300

    v1.1

--- cypress/e2e/pagina-home.cy.js
+++ cypress/e2e/pagina-home.cy.js
@@ -56,8 +56,5 @@ describe('Teste quem ama adota', () => {
     it('Verifica mensagem de texto', () =>{
         cy.contains('p', 'Adotar pode mudar uma vida. Que tal buscar seu novo melhor amigo hoje? Vem com a gente!').should('be.visible')
     })
- 
   });
 
-/*
-*/
