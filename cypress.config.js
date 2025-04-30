6ba65a3396694773c9f9c2260329563abb8a9c34
unknown <gabrielrjustin95@outlook.com>
2025-04-29 21:59:30 -0300

    configuração

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,8 @@ module.exports = defineConfig({
   defaultCommandTimeout: 5000,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+     // implement node event listeners here
     },
+    video: true,
   },
 });
--- cypress/e2e/cadastro-correto.cy.js
+++ cypress/e2e/cadastro-correto.cy.js
@@ -1,9 +1,11 @@
 describe('Página de Cadastro', () => {
-  it('Deve preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
+  beforeEach(() => {
     cy.visit('https://adopet-frontend-cypress.vercel.app');
     cy.get('[data-test="register-button"]').click();
+  });
+  it('Deve preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
     cy.get('[data-test="input-name"]').type('Gabriel TESTE');
-    cy.get('[data-test="input-email"]').type('gabriel.justin@5455155este.com.br');
+    cy.get('[data-test="input-email"]').type('gabriel.justin@5x455155este.com.br');
     cy.get('[data-test="input-password"]').type('Gabrielteste@1');
     cy.get('[data-test="input-confirm-password"]').type('Gabrielteste@1');
     cy.get('[data-test="submit-button"]').click();
