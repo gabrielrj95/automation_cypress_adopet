9874bfe9e5dcf4026a78181d8e77d53c1cb41396
unknown <gabrielrjustin95@outlook.com>
2025-08-07 15:13:07 -0300

    a

--- automation_cypress_adopet/cypress/e2e/cadastro-incorreto.cy.js
+++ automation_cypress_adopet/cypress/e2e/cadastro-incorreto.cy.js
@@ -9,5 +9,6 @@ describe('Página de Cadastro', () => {
     cy.contains('É necessário informar um endereço de email').should('be.visible');
     cy.contains('Crie uma senha').should('be.visible');
     cy.contains('Repita a senha criada acima').should('be.visible'); 
+    
   })
 }) 
